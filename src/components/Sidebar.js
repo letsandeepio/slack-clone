a041f0d58c7a3963d3b46caedef05244b84a4e4a
letsandeepio <sandeepchopra7@gmail.com>
2020-08-28 19:51:20 -0400

    implement dynamic listing of channels from db

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Sidebar.css';
 
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
@@ -15,7 +15,19 @@ import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 
+import db from '../firebase';
+
 function Sidebar() {
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => {
+    db.collection('rooms').onSnapshot((snapshot) => {
+      setChannels(
+        snapshot.docs.map((doc) => ({ id: doc.id, name: doc.data().name }))
+      );
+    });
+  }, []);
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -40,6 +52,9 @@ function Sidebar() {
       <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
       <hr />
       <SidebarOption Icon={AddIcon} title="Add Channel" />
+      {channels.map((channel) => (
+        <SidebarOption key={channel.id} title={channel.name} />
+      ))}
     </div>
   );
 }
